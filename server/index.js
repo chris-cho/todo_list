616dee9e8a8ec338479ad01222a241d78e50671a
Chris Cho <hyunsangc@gmail.com>
2021-10-18 20:24:48 -0400

    add body-parser, cookie-session and /test route

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 "use strict";
 
 // Basic express setup:
-
-const PORT          = 3000;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const cookieSession = require("cookie-session");
 const app           = express();
+
+// app.use(cookieSession({
+//   name: 'session',
+//   keys: [/* secret keys */],
+
+//   // Cookie Options
+//   maxAge: 24 * 60 * 60 * 1000 // 24 hours
+// }));
+
+app.get('/test', (req, res) => {
+  res.send('Hello World!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, (err) => console.log(err || `listening on port ${port} 😎`));
