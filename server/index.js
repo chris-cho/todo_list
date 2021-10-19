b22f93b01c063b801a6034daacc184708089e56f
Chris Cho <hyunsangc@gmail.com>
2021-10-18 20:09:57 -0400

    add bodyParser and cookieSession to express

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
+const PORT          = 3000;
 const express       = require("express");
 const bodyParser    = require("body-parser");
+const cookieSession = require("cookie-session");
 const app           = express();
