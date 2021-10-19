74ce3f658d19dfa6a58931cac19a5291c94276dc
Chris Cho <hyunsangc@gmail.com>
2021-10-18 20:06:07 -0400

    add index.js with basic express setup

---
+++ server/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Basic express setup:
+
+const PORT          = 8080;
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const app           = express();
